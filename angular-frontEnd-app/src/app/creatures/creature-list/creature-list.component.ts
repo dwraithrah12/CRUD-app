2c9cc1e4cad196c124afcbd7977863bf1f3d0970
Robert Jackson <dwraithrah@gmail.com>
2020-02-03 12:08:53 -0600

    added ngFor to creature-list.component.html to display the contents of the hard coded array element. changed image in creature-list.component.ts since the prior one did not support the linking used. moved code around in creatures.component.html to change the aesthetic, putting the creature list on the left and the creature attributes on the right.

--- angular-frontEnd-app/src/app/creatures/creature-list/creature-list.component.ts
+++ angular-frontEnd-app/src/app/creatures/creature-list/creature-list.component.ts
@@ -8,7 +8,7 @@ import{ Creature } from '../creatures.model';
 })
 export class CreatureListComponent{
   creatures:Creature[]=[
-    new Creature('Hot Elf', 'Elf', 'Sexy as hell elf', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F956706&psig=AOvVaw3SAtLpiv1XYnQKqGIgUapO&ust=1580836672915000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCBvtrxtecCFQAAAAAdAAAAABAE')
+    new Creature('Hot Elf', 'Elf', 'Sexy as hell elf', 'https://cdn.pixabay.com/photo/2017/12/06/08/39/dragon-3001238_960_720.png')
   ]
 
 }
