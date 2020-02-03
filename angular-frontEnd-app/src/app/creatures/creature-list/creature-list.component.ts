26b8f647183948496bcffa7886e7c8f5e8833034
Robert Jackson <dwraithrah@gmail.com>
2020-02-03 11:35:00 -0600

    Added code to creatures.component.html. Had it display creature-list.component.html to see how it came out in appearance (minus image since that isn't populated yet). Added code to creature-list.component.html to create a button that will eventually allow the addition of new creatures, and also created a sample template of how creature information will appear on the list. Created an array in creature-list that will hold an array of the class Creature from creatures.model.ts   I populated the array with a hard coded entry to test with next update.

--- angular-frontEnd-app/src/app/creatures/creature-list/creature-list.component.ts
+++ angular-frontEnd-app/src/app/creatures/creature-list/creature-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ Creature } from '../creatures.model';
 
 @Component({
   selector: 'app-creature-list',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./creature-list.component.css']
 })
 export class CreatureListComponent{
+  creatures:Creature[]=[
+    new Creature('Hot Elf', 'Elf', 'Sexy as hell elf', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F956706&psig=AOvVaw3SAtLpiv1XYnQKqGIgUapO&ust=1580836672915000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCBvtrxtecCFQAAAAAdAAAAABAE')
+  ]
+
 }
